d22dd30b1d347b3a639a0693f3fd3e3f58252f54
Haki Bejtullahi <haki.bejtullahi14.95@gmail.com>
2023-04-09 17:05:32 +0200

    example 3 complete

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,26 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get('/api/3-airtable');
+    result.innerHTML = data
+      .map((prod) => {
+        const { url, id, name, price } = prod;
+        return `
+  <a href="product.html?id=${id}" class="product">
+   <img src="${url}" alt="${name}" />
+   <div class="info">
+    <h5>${name}</h5>
+    <h5 class="price">${price}</h5>
+    </div>
+  </a>
+  `;
+      })
+      .join('');
+  } catch (error) {
+    result.innerHTML =
+      '<h2>Sorry there was a problem with the server. Please try again later.</h2>';
+  }
+};
+
+fetchData();
---
+++ functions/3-airtable.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appVLcjLWUruklz8M')
+  .table('products');
+
+exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters;
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id);
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`,
+        };
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `server error`,
+      };
+    }
+  }
+  try {
+    const { records } = await airtable.list();
+    const products = records.map((product) => {
+      const { id } = product;
+      const { name, image, price } = product.fields;
+      const url = image[0].url;
+      return { id, name, url, price };
+    });
+    return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: 500,
+      body: 'general server error',
+    };
+  }
+};
